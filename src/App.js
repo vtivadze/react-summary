2f52516f58c930b892efbba64acd0df4fd7cfe26
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-06-27 03:08:15 +0400

    Create routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { Route, Switch } from "react-router-dom";
+
+import AllMeetupPage from './pages/AllMeetups';
+import FavoritesPage from './pages/Favorites';
+import NewMeetupPage from './pages/NewMeetup';
+
 function App() {
-  return (
-    <div>Hello</div>
-  );
+  return <div>
+    <Switch>
+      <Route path='/' exact>
+        <AllMeetupPage />
+      </Route>
+      <Route path='/favorites'>
+        <FavoritesPage />
+      </Route>
+      <Route path='/new-meetup'>
+        <NewMeetupPage />
+      </Route>
+    </Switch>
+  </div>;
 }
 
 export default App;
